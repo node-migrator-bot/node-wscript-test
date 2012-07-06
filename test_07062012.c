9a76cdd449fbe90584d3b6a5f640f2146a843c6f
Farrin Reid <blakmatrix@gmail.com>
2012-07-06 15:44:00 -0800

    added tests for nodemigrator bot for fs{exists,sexitsSync} and tty.setRawMode

---
+++ test_07062012.c
@@ -0,0 +1,11 @@
+path.exists
+path.exists;
+unless path.exists();
+path.exists(ignoreFilePath, function (exists) {};
+path.exists  (ignoreFilePath, function (exists) {
+path.exists(oldIgnoreFilePath, function (exists) {
+path.existsSync
+if (path.existsSync(dir + '/' + args[i])) {
+if (path.existsSync(flag)) fs.unlinkSync(flag);
+tty.setRawMode
+tty.setRawMode(true);
